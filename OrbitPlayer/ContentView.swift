1d4a2c325eac544b0a820ecbdce9d733f6450cb5
jivison <john@ivison.org>
2021-04-30 18:14:14 -0700

    Initial Commit

---
+++ OrbitPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrbitPlayer
+//
+//  Created by John Ivison on 2021-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OrbitPlayer/OrbitPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitPlayerApp.swift
+//  OrbitPlayer
+//
+//  Created by John Ivison on 2021-04-30.
+//
+
+import SwiftUI
+
+@main
+struct OrbitPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
